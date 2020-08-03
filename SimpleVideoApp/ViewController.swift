9e34dbd7f9e057e1ba284e2cfd432dcdc12f5cab
杉山和輝 <s.kgmgmjtd579@gmail.com>
2020-08-04 01:54:21 +0900

    first commit

---
+++ SimpleVideoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleVideoApp
+//
+//  Created by 杉山和輝 on 2020/08/04.
+//  Copyright © 2020 杉山和輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
